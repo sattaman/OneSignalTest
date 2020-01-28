92a95f5628edada84ba359a01746fe13e49f2a0b
Thomas Sanderson <thomas.sanderson@reedbusiness.com>
2020-01-28 15:23:12 +0000

    add android config

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Test push notifications!!</Text>
-    </View> 
-  );
+export default class App extends Component {
+
+constructor(properties) {
+    super(properties);
+    console.log('OneSignal.init')
+    OneSignal.init("YOUR_ONESIGNAL_APPID");
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Test push notifications!!</Text>
+      </View> 
+    );
+}
 }
 
 const styles = StyleSheet.create({
