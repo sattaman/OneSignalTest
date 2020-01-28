233b3591fe9cd6b12fdb6d39c332d78ed843b7ca
Thomas Sanderson <thomas.sanderson@reedbusiness.com>
2020-01-28 08:54:25 +0000

    add ios extension for push images

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Thomas Sanderson on 28/01/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
